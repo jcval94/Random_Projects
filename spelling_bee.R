9c72af1022858af6d43df909dd0b82c2be48803f
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2020-02-16 01:25:04 -0600

    Update and rename spellin_bee.R to spelling_bee.R
    beed

--- spelling_bee.R
+++ spelling_bee.R
@@ -11,7 +11,7 @@ palabras<-tibble(word=read_lines("https://norvig.com/ngrams/enable1.txt"))
 palabras
 
 
-letras<-c("a","p","x","m","e","l")
+letras<-c("a","p","x","m","e","l","g")
 letra_central<-"g"
 #Palabras que contengan g
 palabras_g<-palabras[str_detect(palabras[[1]],letra_central),]
@@ -20,8 +20,18 @@ palabras_g<-palabras[str_detect(palabras[[1]],letra_central),]
 #longitud de la palabra >=4
 palabras_g4<-palabras_g[str_length(palabras_g[[1]]) >=4,]
 
-palabras_g4 %>% mutate(caracteres=str_length(word)) %>%
+palabras2<-palabras_g4 %>% mutate(caracteres=str_length(word)) %>%
   mutate(puntos=ifelse(caracteres==4,1,caracteres)) %>%
   arrange(desc(puntos))
 
+palabras2[["letras"]]<-strsplit(palabras2[["word"]],"")
+
+#Quita estas letras a este conjunto de letras
+setdiff(palabras2[["letras"]][[1000]],letras)
+
+#Las palabras que se quedaron sin letras es porque
+#les quité todas
+palabras2[["Let_inval"]]<-map(palabras2[["letras"]],setdiff,letras)
+
+palabras2[["Let_inval_len"]]<-map_int(Let_inval,length)
 
