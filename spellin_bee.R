5d3e8ac9ca9298fc01ff380196222ca5980c52cb
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2020-02-15 23:57:24 -0600

    Update spellin_bee.R
    fass

--- spellin_bee.R
+++ spellin_bee.R
@@ -12,11 +12,16 @@ palabras
 
 
 letras<-c("a","p","x","m","e","l")
+letra_central<-"g"
 #Palabras que contengan g
-palabras_g<-palabras[str_detect(palabras[[1]],"g"),]
+palabras_g<-palabras[str_detect(palabras[[1]],letra_central),]
 
 
 #longitud de la palabra >=4
 palabras_g4<-palabras_g[str_length(palabras_g[[1]]) >=4,]
+
+palabras_g4 %>% mutate(caracteres=str_length(word)) %>%
+  mutate(puntos=ifelse(caracteres==4,1,caracteres)) %>%
+  arrange(desc(puntos))
 
 
