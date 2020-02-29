636861da0c6d1da4e964712cd665d60b349d8b1f
Carlos xD <jcval94@gmail.com>
2020-02-29 00:21:09 -0600

    plot_corr

--- h_mOVIES.R
+++ h_mOVIES.R
@@ -8,4 +8,36 @@ horror_movies
 
 #Mejores pelis
 
-horror_movies %>% arrange(desc(review_rating))
+HM_1<-horror_movies %>% arrange(desc(review_rating)) %>%
+  extract(title,"anio","\\((\\d\\d\\d\\d)\\)",remove=F,convert = T)
+
+HM_1$budget<-parse_number(HM_1$budget)
+
+HM_1 %>% ggplot(aes(anio))+geom_histogram()
+
+#La mayoría de las pelis están depués de 2010
+
+HM_1 %>% count(genres,sort = T)
+
+HM_1 %>% count(language,sort = T)
+
+#Retiramos el formato
+HM_1 %>% count(budget,sort = T) %>%
+  ggplot(aes(budget))+geom_histogram()+
+  scale_x_log10(labels=scales::dollar)
+
+#Peliculas con más presupuesto tienen mejor rating?
+
+
+HM_1 %>% ggplot(aes(budget,review_rating)) +
+  geom_point() + scale_x_log10(labels=scales::dollar)+
+  geom_smooth(method = "glm")
+#No parece haber una correlaci{on}
+
+HM_1 %>% select(budget,review_rating) %>% 
+  filter(!is.na(budget) & !is.na(review_rating)) %>%
+  cor()
+#En efecto, es muy baja la correlaci{on}
+
+
+
