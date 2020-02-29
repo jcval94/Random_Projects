a4f63bdf5eb93c25895a2236eba1041232b8fa84
Carlos xD <jcval94@gmail.com>
2020-02-28 23:56:23 -0600

    top pelis

--- h_mOVIES.R
+++ h_mOVIES.R
@@ -1,3 +1,11 @@
 #https://www.youtube.com/watch?v=yFRSTlk3kRQ
 
 #Hrrm movies pred
+horror_movies <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-22/horror_movies.csv")
+
+library(tidyverse)
+horror_movies
+
+#Mejores pelis
+
+horror_movies %>% arrange(desc(review_rating))
