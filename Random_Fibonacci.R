5c19efcaf3e97229f6da97d9605bfd316197c652
Carlos xD <jcval94@gmail.com>
2020-03-08 12:50:40 -0600

    random naccci 2

--- Random_Fibonacci.R
+++ Random_Fibonacci.R
@@ -1,19 +1,32 @@
 
 
 f_b<-function(n){
-  if(!is.integer(n) & n<0){
+  if(!is.integer(n) & any(c(n<0,n==Inf))){
     return(warning("n must be a Natural number"))
   }
-  m_p<-rbinom(n,size=1,prob = 1/2)
   ini<-c(0,1)
   operacion<-function(k,m_p_,n){
-    n<-n-1
-    if (m_p_==0) {
-      return(sum(k))
-    }else{
-      return(k[1]-k[2])
+    m_p<-rbinom(1,size=1,prob = 1/2)
+    if(n!=0){
+      if (m_p_==0) {
+        p<-(sum(k))
+      }else{
+        p<-(k[1]-k[2])
+      }
+      n<-n-1
+      print(c(n,p))
+      p<-operacion(c(k[2],p),m_p,n)
     }
+    else{
+      p<-0
+    }
+    return(p)
   }
+  
+  operacion(ini,m_p,n)
+  
+  
+  
   
   
 }
