df9fd00f094fbeab84e1389658451943c89b4e48
Carlos xD <jcval94@gmail.com>
2020-03-09 00:27:06 -0600

    acumul

--- Random_Fibonacci.R
+++ Random_Fibonacci.R
@@ -1,32 +1,41 @@
 #https://www.youtube.com/watch?v=ELA8gNNMHoU
 
-f_b<-function(n){
-  if(!is.integer(n) & any(c(n<0,n==Inf))){
-    return(warning("n must be a Natural number"))
+f_b<-function(N){
+  if(!is.integer(N) & any(c(N<0,N==Inf))){
+    return(warning("N must be a Natural number"))
   }
   ini<-c(0,1)
-  operacion<-function(k,m_p_,n){
-    m_p<-rbinom(1,size=1,prob = 1/2)
+  acumulado<-acumul<-vector(mode = "list", length = N)
+  
+  operacion<-function(k,n,acumul){
+    #if(missing(acumul)){acumul<-acumulado}
+    m_p_<-rbinom(1,size=1,prob = 1/2)
     if(n!=0){
       if (m_p_==0) {
         p<-(sum(k))
       }else{
         p<-(k[1]-k[2])
       }
+      iter<-N-n+1
+      print(c(iter,p))
+      acumul[[iter]]<-c(iter,p)
       n<-n-1
-      print(c(n,p))
-      p<-operacion(c(k[2],p),m_p,n)
-    }
-    else{
+      p<-operacion(c(k[2],p),n,acumul)
+      print(paste0("A",acumul[[iter]]))
+    }else{
       p<-0
     }
-    return(p)
+    return(acumul)
   }
   
-  operacion(ini,m_p,n)
-  
-  n<-250000
-  
-  
-  
+  seee<-operacion(k=ini,n=N,acumul = acumulado)
+  return(acumul)
 }
+
+  
+  
+n<-1000
+
+l1<-f_b(n)
+
+
