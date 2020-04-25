40b8686667fc673650cd5b1e2922e47a85169182
Carlos xD <jcval94@gmail.com>
2020-04-25 15:57:16 -0500

    lifegame fun finished

--- lifegame.R
+++ lifegame.R
@@ -14,16 +14,32 @@ Iter<-function(m)
   alto <- dim(m)[1] + 2
   ancho <- dim(m)[2] + 2
   
-  m2 <- matrix(integer(alto*ancho), alto, ancho)
-  for (i in 2:(alto-1)) {
-    for (j in 2:(ancho-1)) {
-      # print(c(i,j))
-      # Reglas
-        ##m2[i, j] <- m2[i, j] + 1
-        
-        
-      
+  m_aux <- m2 <- matrix(integer(alto*ancho), alto, ancho)
+  for (i in 1:(alto-2)) {
+    for (j in 1:(ancho-2)) {
+      m2[i+1,j+1] <- m[i,j]
     }
   }
-  return(m2[-c(1,len),-c(1,len)])
+  
+  for (i in 2:(alto-1)) {
+    for (j in 2:(ancho-1)) {
+      # print(c(i,j));m2[i, j] <- m2[i, j] + 1
+      # Valoración
+      vivos <- c(m2[i-1,j-1] == 1, m2[i,j-1] == 1, m2[i+1,j-1] == 1,
+        m2[i-1,j] == 1, m2[i+1,j] == 1,
+        m2[i-1,j+1] == 1, m2[i,j+1] == 1, m2[i+1,j+1] == 1)
+      
+      #Si est{a viva y tiene dos o tres vecinas vivas, sigue viva
+      if(m2[i,j] == 1 & sum(vivos) %in% c(2,3)){
+        m_aux[i,j] = 1
+      }else if(m2[i,j] == 0 & sum(vivos) == 3){
+        #Si tiene exactamente 3 vecinas vivas revive
+        m_aux[i,j] = 1
+      }else{
+        #Otro caso muere
+        m_aux[i,j] = 0
+      }
+    }
+  }
+  return(m_aux[-c(1,len),-c(1,len)])
 }
